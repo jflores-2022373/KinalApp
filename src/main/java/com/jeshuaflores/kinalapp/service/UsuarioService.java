3d5c19f1efce44bb55570d3706e92bec4994bc30
Jeshua Flores <jflores-2022373@kinal.edu.gt>
2026-05-18 20:30:39 -0600

    Creacion de UsuarioService

--- src/main/java/com/jeshuaflores/kinalapp/service/UsuarioService.java
+++ src/main/java/com/jeshuaflores/kinalapp/service/UsuarioService.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+// ===================================================================
+// CAPA DE NEGOCIO: ADMINISTRACIÓN Y CIFRADO DE USUARIOS DEL SISTEMA
+// ===================================================================
 @Service
 public class UsuarioService {
 
