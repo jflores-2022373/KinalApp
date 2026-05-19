6e5605290e811f00706cfae1978e5ba6246444c0
Jeshua Flores <jflores-2022373@kinal.edu.gt>
2026-05-18 20:17:42 -0600

    Creacion de VentaRepository

--- src/main/java/com/jeshuaflores/kinalapp/repository/VentaRepository.java
+++ src/main/java/com/jeshuaflores/kinalapp/repository/VentaRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+// Manejador transaccional para el almacenamiento y reporte de cabeceras de venta
 public interface VentaRepository
         extends JpaRepository<Venta, Long> {
 
