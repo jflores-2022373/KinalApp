318514c1c9017719aa3a5bf0921a8dd7c0119dd4
Jeshua Flores <jflores-2022373@kinal.edu.gt>
2026-05-18 20:18:37 -0600

    Creacion de DetalleVentaRepository

--- src/main/java/com/jeshuaflores/kinalapp/repository/DetalleVentaRepository.java
+++ src/main/java/com/jeshuaflores/kinalapp/repository/DetalleVentaRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+// Capa de persistencia dedicada al almacenamiento masivo de los desgloses de facturas
 public interface DetalleVentaRepository
         extends JpaRepository<DetalleVenta, Long> {
 
