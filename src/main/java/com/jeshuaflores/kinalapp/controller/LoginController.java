b32e0a09805e858ff389d2203bf25afc7ed6fb7b
Jeshua Flores <jflores-2022373@kinal.edu.gt>
2026-05-18 20:48:08 -0600

    Configuracion de LoginController

--- src/main/java/com/jeshuaflores/kinalapp/controller/LoginController.java
+++ src/main/java/com/jeshuaflores/kinalapp/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.jeshuaflores.kinalapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+// Gestión de peticiones de inicio de sesión y renderizado del login customizado
 @Controller
 public class LoginController {
 
