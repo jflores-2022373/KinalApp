eeba60a49b5690474a3132e725dd4f36f67c01bc
Jeshua Flores <jflores-2022373@kinal.edu.gt>
2026-05-18 20:15:03 -0600

    Creacion de ProductoRepository

--- src/main/java/com/jeshuaflores/kinalapp/repository/ProductoRepository.java
+++ src/main/java/com/jeshuaflores/kinalapp/repository/ProductoRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+// Interfaz de abstracción de datos para consultas sobre el catálogo de productos
 public interface ProductoRepository
         extends JpaRepository<Producto, Long> {
 
