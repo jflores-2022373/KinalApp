7345c587eb94c68f96200bb4ca80bd2eabe88050
Jeshua Flores <jflores-2022373@kinal.edu.gt>
2026-05-18 20:14:20 -0600

    Creacion de ClienteRepository

--- src/main/java/com/jeshuaflores/kinalapp/repository/ClienteRepository.java
+++ src/main/java/com/jeshuaflores/kinalapp/repository/ClienteRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+// Componente de acceso a datos (DAO) para operaciones de persistencia del Cliente
 public interface ClienteRepository
         extends JpaRepository<Cliente, Long> {
 
