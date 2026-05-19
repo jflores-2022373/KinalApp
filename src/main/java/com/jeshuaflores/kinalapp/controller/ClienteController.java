e4442e5db1a10aff60a74f387b5bcca5685c1610
Jeshua Flores <jflores-2022373@kinal.edu.gt>
2026-05-18 19:29:03 -0600

    Configuracion de paquetes principales del sistema

--- src/main/java/com/jeshuaflores/kinalapp/controller/ClienteController.java
+++ src/main/java/com/jeshuaflores/kinalapp/controller/ClienteController.java
@@ -14,6 +14,7 @@ import org.springframework.validation.BindingResult;
 
 import org.springframework.web.bind.annotation.*;
 
+// CONTROLADOR DE ARQUITECTURA MVC - SEGMENTO: CLIENTES
 @Controller
 @RequestMapping("/clientes")
 public class ClienteController {
